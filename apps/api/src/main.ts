ff57cab32cef9b28931cb8937b22a5f229ad2a60
Sergio Pedrero Benedi <sergiotecnologia92@gmail.com>
2021-09-10 18:11:34 +0200

    ADD - Postgres db to docker compose & node package

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,13 +8,15 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import "reflect-metadata";
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
-    Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
+    Logger.log(`Listening at http://localhost:${port}/${globalPrefix}`);
   });
 }
 
